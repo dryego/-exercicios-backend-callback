bef390492e371332b675dae74abb4923c7e5c2a1
Vitor Vidal <87544179+vitorfrsvidal@users.noreply.github.com>
2022-03-15 14:28:34 -0300

    Exercícios

--- 05/index.js
+++ 05/index.js
@@ -1,7 +1,8 @@
 
 
-let alarme = "18:45:00";
-let tempoAlarmandoEmSegundos = 10;
+let tempoParaAlarmarEmSegundos = 10;
+let tempoAlarmandoEmSegundos = 5;
 let tempoSonecaEmSegundos = 10;
+let numeroDeSonecas = 2;
 
 
